6e420d732ad9f64eef7be5ce7b3ecb3aa3e493ab
aplacaba <alacaba@fastmail.com>
2020-05-26 20:09:46 +0800

    Use JetBrains Mono as font

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -275,6 +275,16 @@
 (use-package modus-operandi-theme
   :ensure t)
 
+
+;; elfeed
+(use-package elfeed
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x w") 'elfeed)
+  (setq elfeed-feeds
+        '(("http://news.ycombinator.com/rss" HN)
+          ("https://protesilaos.com/codelog.xml" prot))))
+
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
@@ -297,7 +307,21 @@
 (define-key global-map [remap list-buffers] 'helm-mini)
 (file-extensions)
 (setup-eglot-lsp)
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-10"))
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (elfeed web-mode use-package smartparens rust-mode ruby-end pdf-tools neotree modus-vivendi-theme modus-operandi-theme markdown-mode magit indent-guide helm-rg helm-projectile fsharp-mode flycheck emmet-mode elixir-mode cycle-themes company base16-theme avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
