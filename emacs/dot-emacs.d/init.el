cc95847842c69c4d51f5ce43e05e80f63fcb7fa3
aplacaba <alacaba@fastmail.com>
2020-07-27 10:39:28 +0800

    Added ivy, counsel, swiper

--- emacs/dot-emacs.d/init.el
+++ emacs/dot-emacs.d/init.el
@@ -237,10 +237,38 @@
 (use-package json-mode
   :ensure t)
 
+(use-package swiper
+  :ensure t)
+
+(use-package counsel
+  :ensure t)
+
+(use-package ivy
+  :ensure t
+  :config
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
+
 ;; projectile
 (use-package projectile
   :ensure t
   :config
+  (setq projectile-completion-system 'ivy)
   (projectile-mode +1)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
@@ -305,7 +333,8 @@
   (persp-mode))
 
 (use-package ibuffer-projectile
-  :ensure t)
+  :ensure t
+  :defer)
 
 (use-package ibuffer
   :config
