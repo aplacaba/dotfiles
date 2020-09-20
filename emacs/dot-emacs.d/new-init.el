e48124ef0fd18eca02aa90f47041472d0fc8bcac
aplacaba <alacaba@fastmail.com>
2020-09-20 19:22:57 +0800

    Added prot's emacs init

---
+++ emacs/dot-emacs.d/new-init.el
@@ -0,0 +1,42 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/")
+             '("gnu" . "https://elpa.gnu.org/packages/"))
+
+;; Initialise the packages, avoiding a re-initialisation.
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
+
+;; Make sure `use-package' is available.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Configure `use-package' prior to loading it.
+(eval-and-compile
+  (setq use-package-always-ensure nil)
+  (setq use-package-always-defer nil)
+  (setq use-package-always-demand nil)
+  (setq use-package-expand-minimally nil)
+  (setq use-package-enable-imenu-support t)
+  (setq use-package-compute-statistics t)
+  ;; The following is VERY IMPORTANT.  Write hooks using their real name
+  ;; instead of a shorter version: after-init ==> `after-init-hook'.
+  ;;
+  ;; This is to empower help commands with their contextual awareness,
+  ;; such as `describe-symbol'.
+  (setq use-package-hook-name-suffix nil))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package org)
+
+(let* ((conf "~/.emacs.d/emacs-init")
+       (el (concat conf ".el"))
+       (org (concat conf ".org")))
+  (if (file-exists-p el)
+      (load-file el)
+    (org-babel-load-file org)))
