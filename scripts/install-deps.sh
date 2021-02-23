764983240536323797febb1913b4504fa0f49afc
aplacaba <alacaba@fastmail.com>
2021-02-23 14:28:27 +0800

    consolidate deps and language specific packages

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,19 +3,12 @@
 sudo dnf update -y
 
 # ruby deps
-echo "Installing ruby deps"
-sudo dnf install -y gcc make bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
+echo "Installing deps"
+sudo dnf install -y gcc make bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel make automake autoconf gcc gcc-c++ ImageMagick libpng-devel zlib-devel poppler-glib-devel libvterm
 
 # postgres, redis
 echo "Installing postgres, redis, erlang"
-sudo dnf install -y postgresql postgresql-devel redis curl git erlang clojure
-
-# vterm, pdf-tools
-echo "Installing vterm, pdf-tools"
-sudo dnf install -y make automake autoconf gcc gcc-c++ ImageMagick libpng-devel zlib-devel poppler-glib-devel libvterm
-
-echo "Setting up erlang"
-sudo dnf install -y erlang erlang-devel
+sudo dnf install -y postgresql postgresql-devel redis curl git erlang clojure erlang
 
 echo "Hack fonts"
 sudo dnf copr enable zawertun/hack-fonts
@@ -30,15 +23,11 @@ RUBY_VERSION=2.6.6
 NODE_VERSION=14.15.4
 EX_VERSION=1.11.3
 
-# ruby
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-
-# nodejs
-bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+# ruby, node, elixir
+asdf plugin-add ruby   https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-
-# elixir
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 
 asdf install ruby $RUBY_VERSION
 asdf install nodejs $NODE_VERSION
